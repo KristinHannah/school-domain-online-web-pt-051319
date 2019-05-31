39dcacef27359885793a75381951e9bcfab26623
Kristin Kraemer <kristinhannahk@gmail.com>
2019-05-31 00:16:58 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -12,8 +12,7 @@ class School
   def add_student(student_name, grade)
     @student_name = student_name
     @grade = grade 
-    
-    @roster[:@grade] << @student_name
+    @roster[:@grade] = @student_name
   end 
 
 end 
